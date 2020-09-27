18e0a76da20e34f1369f5ff22ac9f256d7b9c125
RayeEThompson <rachael.elizabeth.thompson@gmail.com>
2020-09-27 08:11:21 -0500

    Updates

---
+++ precache-manifest.3c3644f308b92e61abd960c9d3336bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998ff244bb8aed46bbf2551088a570c8",
+    "url": "/RollDice/index.html"
+  },
+  {
+    "revision": "1672f08245e7cad94926",
+    "url": "/RollDice/static/css/main.69fd8b09.chunk.css"
+  },
+  {
+    "revision": "12f8f360e57227cfe91a",
+    "url": "/RollDice/static/js/2.3a1a4e2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RollDice/static/js/2.3a1a4e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1672f08245e7cad94926",
+    "url": "/RollDice/static/js/main.66b10fd2.chunk.js"
+  },
+  {
+    "revision": "f1f7abec5d7bd92a8813",
+    "url": "/RollDice/static/js/runtime-main.6ef7bdd7.js"
+  }
+]);
